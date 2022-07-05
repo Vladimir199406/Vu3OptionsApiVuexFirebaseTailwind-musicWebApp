cbd6c0535dd6e39f000be9dfc7ac8a17fd57e91d
vo <vovasso550@rambler.ru>
2022-07-05 16:00:23 +0300

    refactoring register logic

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,35 @@
 import { createStore } from 'vuex';
+import { auth, usersCollection } from '@/includes/firebase';
 
 export default createStore({
   state: {
     authModalShow: false,
+    userLoggedIn: false,
   },
   mutations: {
     toggleAuthModal: (state) => {
       state.authModalShow = !state.authModalShow;
     },
+    toggleAuth(state) {
+      state.userLoggedIn = !state.userLoggedIn;
+    },
   },
-  actions: {},
+  actions: {
+    async register({ commit }, formData) {
+      await auth.createUserWithEmailAndPassword(
+        formData.email, formData.password,
+      );
+
+      await usersCollection.add({
+        name: formData.name,
+        email: formData.email,
+        age: formData.age,
+        country: formData.country,
+      });
+
+      commit('toggleAuth');
+    },
+  },
   modules: {},
   getters: {
   //  authModalShow: (state) => state.authModalShow,
