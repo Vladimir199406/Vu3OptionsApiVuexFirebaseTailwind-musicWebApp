847bd131f4dc7952c58be14199bb7ced64f37b97
vo <vovasso550@rambler.ru>
2022-07-13 21:07:29 +0300

    song progress bar

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex';
 import { auth, usersCollection } from '@/includes/firebase';
 import { Howl } from 'howler';
+import helper from '@/includes/helper';
 
 export default createStore({
   state: {
@@ -8,6 +9,9 @@ export default createStore({
     userLoggedIn: false,
     currentSong: {},
     sound: {},
+    seek: '00:00',
+    duration: '00:00',
+    playerProgress: '0%',
   },
   mutations: {
     toggleAuthModal: (state) => {
@@ -22,6 +26,11 @@ export default createStore({
         src: [songData.url],
         html5: true,
       });
+    },
+    updatePosition(state) {
+      state.seek = helper.formatTime(state.sound.seek());
+      state.duration = helper.formatTime(state.sound.duration());
+      state.playerProgress = `${(state.sound.seek() / state.sound.duration()) * 100}%`;
     },
   },
   getters: {
@@ -67,10 +76,20 @@ export default createStore({
       await auth.signOut();
       commit('toggleAuth');
     },
-    async newSong({ commit, state }, songData) {
+    async newSong({ commit, state, dispatch }, songData) {
+      if (state.sound instanceof Howl) {
+        state.sound.unload();
+      }
+
       commit('newSong', songData);
 
       state.sound.play();
+
+      state.sound.on('play', () => {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
+      });
     },
     async toggleAudio({ state }) {
       if (!state.sound.playing) {
@@ -82,6 +101,31 @@ export default createStore({
       } else {
         state.sound.play();
       }
+    },
+    progress({ commit, state, dispatch }) {
+      commit('updatePosition');
+
+      if (state.sound.playing()) {
+        requestAnimationFrame(() => {
+          dispatch('progress');
+        });
+      }
+    },
+    updateSeek({ state, dispatch }, payload) {
+      if (!state.sound.playing) {
+        return;
+      }
+
+      const { x, width } = payload.currentTarget.getBoundingClientRect();
+      const clickX = payload.clientX - x;
+      const percentage = clickX / width;
+      const seconds = state.sound.duration() * percentage;
+
+      state.sound.seek(seconds);
+
+      state.sound.once('seek', () => {
+        dispatch('progress');
+      });
     },
   },
   modules: {},
