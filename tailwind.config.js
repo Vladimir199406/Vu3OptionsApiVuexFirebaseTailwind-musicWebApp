5455ff759967c832a692175b9329f8ddccba070c
vo <vovasso550@rambler.ru>
2022-07-14 17:15:16 +0300

    translation proto setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: { content: ['./public/**/*.html', './src/**/*.vue']},
+  purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {
-    extend: {}
+    extend: {},
   },
   plugins: [],
-}
+};
